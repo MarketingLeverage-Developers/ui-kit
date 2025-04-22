998e3f342c1a2da6fcd3db56347f9b2bbc21906a
dhkim <dhkim@mleverage.co.kr>
2025-04-22 18:20:15 +0900

    feat: 초기 작업 진행중

---
+++ ArticleDetail/ArticleDetail.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Box from '../Box/Box';
+import Title from './Title/Title';
+import SubArea from './SubArea/SubArea';
+import Content from './Content/Content';
+
+type ArticleDetailProps = {
+    children: React.ReactNode;
+};
+
+const ArticleDetail = ({ children }: ArticleDetailProps) => {
+    return <Box>{children}</Box>;
+};
+
+export default ArticleDetail;
+
+ArticleDetail.Title = Title;
+ArticleDetail.SubArea = SubArea;
+ArticleDetail.Content = Content;
