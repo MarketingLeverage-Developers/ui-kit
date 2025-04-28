4c027f63493117345fef7f70c51a3451698257b8
dhkim <dhkim@mleverage.co.kr>
2025-04-28 19:42:30 +0900

    feat: SearchInputA 수정 - onButtonClick 생성

--- components/contents/SearchInput/A/SearchInputA.tsx
+++ components/contents/SearchInput/A/SearchInputA.tsx
@@ -4,9 +4,9 @@ import { CSSPropertiesWithVars, HexColor } from '@/ui-kit/types';
 import Image from '@/ui-kit/components/contents/Image/Image';
 import MagnifyImage from '@/ui-kit/assets/images/magnify.png';
 
-type SearchInputAProps = InputHTMLAttributes<HTMLInputElement> & { buttonColor?: HexColor };
+type SearchInputAProps = InputHTMLAttributes<HTMLInputElement> & { buttonColor?: HexColor; onButtonClick?: () => void };
 
-const SearchInputA = ({ buttonColor = '#E88731', ...props }: SearchInputAProps) => {
+const SearchInputA = ({ buttonColor = '#E88731', onButtonClick, ...props }: SearchInputAProps) => {
     const cssVariables: CSSPropertiesWithVars = {
         '--button-color': buttonColor,
     };
@@ -14,7 +14,7 @@ const SearchInputA = ({ buttonColor = '#E88731', ...props }: SearchInputAProps) 
     return (
         <div className={styles.SearchInputA} style={{ ...props.style, ...cssVariables }}>
             <input {...props} />
-            <button>
+            <button onClick={onButtonClick}>
                 <Image image={MagnifyImage.src} width={30} height={30} />
             </button>
         </div>
