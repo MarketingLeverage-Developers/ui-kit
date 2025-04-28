fbd78c875ebb5a29e3bb3149df1da6584cb2a7cb
dhkim <dhkim@mleverage.co.kr>
2025-04-28 10:58:57 +0900

    feat: TabU.Item 수정 - color 속성 추가

--- components/contents/Tab/U/Item/Item.tsx
+++ components/contents/Tab/U/Item/Item.tsx
@@ -3,20 +3,26 @@ import styles from './Item.module.scss';
 import TabGroup, { useTabGroup } from '@/headless/TabGroup/TabGroup';
 import Flex from '@/ui-kit/components/layouts/Flex/Flex';
 import Text from '@/ui-kit/components/contents/Text/Text';
+import { CSSPropertiesWithVars, HexColor } from '@/ui-kit/types';
 
 type ItemProps = React.ComponentProps<typeof TabGroup.Item> & {
     label: string;
+    color?: HexColor;
 };
 
-const Item = ({ label, ...props }: ItemProps) => {
+const Item = ({ label, color = '#E88731', ...props }: ItemProps) => {
     const { isActiveTab } = useTabGroup();
 
     const className = classNames(styles.Item, {
         [styles.Active]: isActiveTab(props.value),
     });
 
+    const cssVariables: CSSPropertiesWithVars = {
+        '--color': color,
+    };
+
     return (
-        <TabGroup.Item {...props} style={{ background: 'inherit' }}>
+        <TabGroup.Item {...props} style={{ background: 'inherit', ...cssVariables }}>
             <Flex gap={15} align="center">
                 <div className={className} />
                 <Text size={18}>{label}</Text>
