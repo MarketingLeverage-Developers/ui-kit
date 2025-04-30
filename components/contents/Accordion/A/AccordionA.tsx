bfd6cefd855849bfb054aa284d57e9263ecc67b9
dhkim <dhkim@mleverage.co.kr>
2025-04-30 17:52:43 +0900

    feat: AccordionA 수정 - 화살표 이미지 추가

--- components/contents/Accordion/A/AccordionA.tsx
+++ components/contents/Accordion/A/AccordionA.tsx
@@ -1,7 +1,9 @@
-import Accordion from '@/headless/Accordion/Accordion';
+import Accordion, { useAccordion } from '@/headless/Accordion/Accordion';
 import React from 'react';
 import styles from './AccordionA.module.scss';
-import Flex from '@/ui-kit/components/layouts/Flex/Flex';
+import ArrowUp from '@/ui-kit/assets/images/accordion-arrow-up.svg';
+import ArrowDown from '@/ui-kit/assets/images/accordion-arrow-down.svg';
+import Image from '@/ui-kit/components/contents/Image/Image';
 
 type AccordionAProps = {
     visible: React.ReactNode;
@@ -9,16 +11,33 @@ type AccordionAProps = {
 };
 
 const AccordionA = ({ visible, hidden }: AccordionAProps) => {
+    const { accordionValue } = useAccordion();
+
+    console.log(accordionValue, 'accordionValue');
+
     return (
-        <Accordion>
+        <>
             <Accordion.Box className={styles.Box}>
                 <Accordion.Button className={styles.Button}>
+                    <div className={styles.Absolute}>
+                        <Image image={accordionValue ? ArrowUp.src : ArrowDown.src} />
+                    </div>
                     <Accordion.Visible className={styles.Visible}>{visible}</Accordion.Visible>
                 </Accordion.Button>
                 <Accordion.Hidden className={styles.Hidden}>{hidden}</Accordion.Hidden>
             </Accordion.Box>
-        </Accordion>
+        </>
     );
 };
 
-export default AccordionA;
+export default withProvider(AccordionA);
+
+function withProvider<P extends {}>(WrappedComponent: React.ComponentType<P>): React.FC<P> {
+    return (props: P) => {
+        return (
+            <Accordion>
+                <WrappedComponent {...(props as any)} />
+            </Accordion>
+        );
+    };
+}
