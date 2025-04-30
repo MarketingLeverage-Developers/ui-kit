4286a8627ee0b875109c1faa463de5ef6b8ab828
dhkim <dhkim@mleverage.co.kr>
2025-04-30 16:06:34 +0900

    feat: TabX.Item 수정 - Item 구조, 스타일 수정

--- components/contents/Tab/X/Item/Item.tsx
+++ components/contents/Tab/X/Item/Item.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import styles from './Item.module.scss';
-import Flex from '@/ui-kit/components/layouts/Flex/Flex';
-import Text from '@/ui-kit/components/contents/Text/Text';
 import TabGroup, { useTabGroup } from '@/headless/TabGroup/TabGroup';
 import classNames from 'classnames';
 
 type ItemProps = {
     image: string;
-    label: string;
     value: string;
     onItemClick: (value: string) => void;
 };
 
-const Item = ({ image, label, value, onItemClick }: ItemProps) => {
+const Item = ({ image, value, onItemClick }: ItemProps) => {
     const { isActiveTab } = useTabGroup();
 
-    const className = classNames(styles.ItemBox, {
+    const className = classNames(styles.Item, {
         [styles.Active]: isActiveTab(value),
     });
 
     return (
-        <TabGroup.Item value={value} className={styles.Item} onClick={() => onItemClick(value)}>
-            <Flex direction="column" gap={15} align="center" width={100}>
-                <Flex justify="center" align="center" className={className}>
-                    <img src={image} alt="이미지" />
-                </Flex>
-                <Flex>
-                    <Text size={18} weight={500} color="#111" align="center">
-                        {label}
-                    </Text>
-                </Flex>
-            </Flex>
+        <TabGroup.Item value={value} className={className} onClick={() => onItemClick(value)}>
+            <img src={image} alt="이미지" />
         </TabGroup.Item>
     );
 };
