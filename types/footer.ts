edeacf7bac10a7b082745c6ff9549e7b7f065b6f
dhkim <dhkim@mleverage.co.kr>
2025-05-02 14:54:50 +0900

    feat: Footer 타입 생성

---
+++ types/footer.ts
@@ -0,0 +1,12 @@
+export type Footer = {
+    businessName: string;
+    ceoName: string;
+    businessAddr: string;
+    businessNumber1: string;
+    businessNumber2: string | null;
+    email: string;
+    phone: string;
+    copyright: string;
+    notice: string;
+    logoPath: string | null;
+};
