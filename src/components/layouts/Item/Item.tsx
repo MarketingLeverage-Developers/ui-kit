63a70100f0b7016fae505f59591cc452d62abf14
dhkim <dhkim@mleverage.co.kr>
2025-05-28 20:42:08 +0900

    feat: Item 수정 - width, height 추가

--- src/components/layouts/Item/Item.tsx
+++ src/components/layouts/Item/Item.tsx
@@ -1,23 +1,29 @@
 import React, { HTMLAttributes } from 'react';
 import styles from './Item.module.scss';
 import classNames from 'classnames';
+import { BoxSize } from '@/ui-kit/src/types';
+import { dimensionToVariable } from '@/ui-kit/src/utils';
 
 type ItemProps = HTMLAttributes<HTMLDivElement> & {
     children: React.ReactNode;
     flex?: number;
     mobile?: boolean;
     desktop?: boolean;
+    width?: BoxSize | string;
+    height?: BoxSize | string;
 };
 
 interface CSSPropertiesWithVars extends React.CSSProperties {
     [key: `--${string}`]: string | number;
 }
 
-const Item = ({ flex = 1, mobile, desktop, children, ...props }: ItemProps) => {
+const Item = ({ flex = 1, mobile, desktop, children, width, height, ...props }: ItemProps) => {
     const computedItem = flex;
 
     const cssVariables: CSSPropertiesWithVars = {
         '--flex': computedItem,
+        '--width': dimensionToVariable(width),
+        '--height': dimensionToVariable(height),
     };
 
     const className = classNames(styles.Item, {
