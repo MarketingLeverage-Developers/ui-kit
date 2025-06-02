fd6e995513ff4059492ae705254a1ec7798c096c
mlyykkang <yykkang@mleverage.co.kr>
2025-06-02 16:13:01 +0900

    feat: AccordionF 컴포넌트  full 속성 추가

--- src/components/contents/Accordion/F/AccordionF.tsx
+++ src/components/contents/Accordion/F/AccordionF.tsx
@@ -3,21 +3,24 @@ import React from 'react';
 import styles from './AccordionF.module.scss';
 import Accordion from '@/headless/Accordion/Accordion';
 import Item from './Item/Item';
-import Button from './Visible/Visible';
-import Content from './Hidden/Hidden';
 import Hidden from './Hidden/Hidden';
 import Visible from './Visible/Visible';
+import classNames from 'classnames';
 
 type AccordionFProps = {
     children: React.ReactNode;
     defaultValue: string | { label: string; value: any };
+    full?: boolean;
 };
 
-const AccordionF = ({ children, defaultValue }: AccordionFProps) => {
+const AccordionF = ({ children, defaultValue, full }: AccordionFProps) => {
+    const combinedClass = classNames(styles.Box, {
+        [styles.Full]: full,
+    });
     return (
         <SelectGroup defaultValue={defaultValue}>
             <Accordion>
-                <Accordion.Box className={styles.Box}>{children}</Accordion.Box>
+                <Accordion.Box className={combinedClass}>{children}</Accordion.Box>
             </Accordion>
         </SelectGroup>
     );
