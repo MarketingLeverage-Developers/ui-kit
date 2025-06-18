da28c869bb8bf5ac1104d99d2b1c7846a27fcc87
dhkim <dhkim@mleverage.co.kr>
2025-06-18 11:01:28 +0900

    feat: Content 수정 - bottomBorder, rightBorder 추가

--- src/components/contents/Table/A/Content/Content.tsx
+++ src/components/contents/Table/A/Content/Content.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import styles from './Content.module.scss';
 import { dimensionToString } from '@/ui-kit/src/utils';
+import classNames from 'classnames';
 
 type ContentProps = {
     children?: React.ReactNode;
     align?: 'left' | 'center' | 'right';
     width?: number | string;
+    bottomDivider?: boolean;
+    rightDivider?: boolean;
 };
 
-const Content = ({ width, children, align = 'left' }: ContentProps) => {
+const Content = ({ width, children, align = 'left', bottomDivider, rightDivider }: ContentProps) => {
     const cssVariables: React.CSSProperties = {
         '--width': dimensionToString(width),
         '--align': align,
     } as React.CSSProperties;
 
+    const contentClassName = classNames(styles.Content, {
+        [styles.BottomDivider]: bottomDivider,
+        [styles.RightDivider]: rightDivider,
+    });
+
     return (
-        <div className={styles.Content} style={{ ...cssVariables }}>
+        <div className={contentClassName} style={{ ...cssVariables }}>
             {children}
         </div>
     );
