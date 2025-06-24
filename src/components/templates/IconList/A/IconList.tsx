03a116021b72bc3b5f4d547770fad4adcb377373
mleverage-Intern <intern@mleverage.co.kr>
2025-06-24 11:15:19 +0900

    iconList 수정

--- src/components/templates/IconList/A/IconList.tsx
+++ src/components/templates/IconList/A/IconList.tsx
@@ -4,7 +4,7 @@ import { dimensionToVariable } from '@/ui-kit/src/utils';
 import React from 'react';
 
 interface IconItem {
-    name: string;
+    content: React.ReactNode;
     Icon: string;
 }
 
@@ -25,7 +25,7 @@ const IconList = ({ Icon, width, height }: IconListProps) => {
                 <React.Fragment key={index}>
                     <div key={index} className={styles.FlexItem}>
                         <img src={el.Icon} alt={`icon-${index}`} />
-                        <p>{el.name}</p>
+                        <p>{el.content}</p>
                     </div>
                     {index < Icon.length - 1 && <div className={styles.Divider} />}
                 </React.Fragment>
