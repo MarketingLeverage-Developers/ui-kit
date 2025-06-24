0376f31c0b62dcfaa84bd3f84b4bbf8dcefeda51
dhkim <dhkim@mleverage.co.kr>
2025-06-24 18:01:13 +0900

    feat: TooltipD.Content 수정 - width, height 속성 추가

--- src/components/contents/Tooltip/D/Content/Content.tsx
+++ src/components/contents/Tooltip/D/Content/Content.tsx
@@ -1,11 +1,25 @@
 import Tooltip from '@/headless/Tooltip/Tooltip';
 import React from 'react';
 import styles from './Content.module.scss';
+import { BoxSize, CSSPropertiesWithVars } from '@/ui-kit/src/types';
+import { dimensionToVariable } from '@/ui-kit/src/utils';
 
-type TooltipContentProps = React.ComponentProps<typeof Tooltip.Content>;
+type TooltipContentProps = React.ComponentProps<typeof Tooltip.Content> & {
+    width?: BoxSize | string;
+    height?: BoxSize | string;
+};
 
-const Content = ({ ...props }: TooltipContentProps) => {
-    return <Tooltip.Content {...props} className={styles.Content} />;
+const Content = ({ width = '100%', height = '100%', ...props }: TooltipContentProps) => {
+    console.log('왜이러지?', dimensionToVariable(width));
+
+    const cssVariables: CSSPropertiesWithVars = {
+        '--width': dimensionToVariable(width),
+        '--height': dimensionToVariable(height),
+    };
+
+    console.log('왜??', cssVariables);
+
+    return <Tooltip.Content {...props} className={styles.Content} style={{ ...cssVariables }} />;
 };
 
 export default Content;
