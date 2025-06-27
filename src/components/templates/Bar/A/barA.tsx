c16a08c936bbdb4ce98a236cb601b14bc6d93cda
mleverage-Intern <intern@mleverage.co.kr>
2025-06-27 09:47:11 +0900

    feat: barA 제작

---
+++ src/components/templates/Bar/A/barA.tsx
@@ -0,0 +1,24 @@
+import Image from '@/ui-kit/src/components/contents/Image/Image';
+import styles from './BarA.module.scss';
+
+type BarAProps = {
+    Icon?: string;
+    number?: string;
+    content?: string;
+    highlight?: any;
+};
+
+const BarA = ({ Icon, content, highlight, number }: BarAProps) => {
+    return (
+        <div className={styles.Container}>
+            <div className={styles.Wrapper}>
+                <div className={styles.IconWrap}>
+                    {Icon && <Image image={Icon} />}
+                    {number && number}
+                </div>
+                {highlight ? highlight : <div className={styles.text}>{content}</div>}
+            </div>
+        </div>
+    );
+};
+export default BarA;
