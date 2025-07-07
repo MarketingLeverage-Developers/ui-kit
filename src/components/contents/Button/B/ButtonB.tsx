700824cc3d6ab3ab974024f13fafeb9df44665f6
dhkim <dhkim@mleverage.co.kr>
2025-07-07 16:01:29 +0900

    feat: ButtonB 수정 - 고정값 속성 추가

--- src/components/contents/Button/B/ButtonB.tsx
+++ src/components/contents/Button/B/ButtonB.tsx
@@ -1,19 +1,28 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import styles from './ButtonB.module.scss';
 import classNames from 'classnames';
-import { ContentSize, CSSPropertiesWithVars, HexColor } from '../../../../types';
+import { BoxSize, ContentSize, CSSPropertiesWithVars, FontSize, HexColor, PaddingSize } from '../../../../types';
+import { dimensionToString, dimensionToVariable, spacingToSpace, spacingToString, toFont } from '@/ui-kit/src/utils';
 
 type ButtonBProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     size?: ContentSize;
-    color?: HexColor;
+    bgColor?: HexColor;
     textColor?: HexColor;
     full?: boolean;
+    fontSize?: FontSize | string;
+    padding?: PaddingSize;
+    height?: BoxSize | number;
+    s?: boolean;
 };
 
-const ButtonB = ({ color, textColor, size = 'md', full, ...props }: ButtonBProps) => {
+const ButtonB = ({ color, textColor, size, full, fontSize, height, bgColor, padding, s, ...props }: ButtonBProps) => {
     const cssVariables: CSSPropertiesWithVars = {
         '--color': color,
         '--text-color': textColor,
+        '--background-color': bgColor,
+        '--font-size': s ? dimensionToString(fontSize) : toFont(fontSize),
+        '--padding': s ? spacingToString(padding) : spacingToSpace(padding),
+        '--height': s ? dimensionToString(height) : dimensionToVariable(height),
     };
 
     const combinedStyles = classNames(styles.ButtonB, props.className, {
@@ -25,7 +34,7 @@ const ButtonB = ({ color, textColor, size = 'md', full, ...props }: ButtonBProps
         [styles.Full]: full,
     });
 
-    return <button {...props} className={combinedStyles} style={{ ...cssVariables, ...props.style }} />;
+    return <button {...props} className={combinedStyles} style={{ ...props.style, ...cssVariables }} />;
 };
 
 export default ButtonB;
