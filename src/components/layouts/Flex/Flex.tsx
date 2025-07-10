71d0ecd2ba78249e018f0750c73cb005e79b098c
dhkim <dhkim@mleverage.co.kr>
2025-07-10 09:37:22 +0900

    feat: Flex 수정 - s 속성 적용

--- src/components/layouts/Flex/Flex.tsx
+++ src/components/layouts/Flex/Flex.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './Flex.module.scss';
-import { BoxSize, SpaceSize } from '../../../types';
-import { dimensionToSpace, dimensionToVariable } from '@/ui-kit/src/utils';
-
-type PaddingSize =
-    | SpaceSize
-    | {
-          x?: SpaceSize;
-          y?: SpaceSize;
-          l?: SpaceSize;
-          r?: SpaceSize;
-          t?: SpaceSize;
-          b?: SpaceSize;
-      };
+import { BoxSize, PaddingSize, SpaceSize } from '../../../types';
+import {
+    dimensionToSpace,
+    dimensionToString,
+    dimensionToVariable,
+    spacingToSpace,
+    spacingToString,
+} from '@/ui-kit/src/utils';
 
 export type FlexProps = React.HTMLAttributes<HTMLDivElement> & {
     children: React.ReactNode;
@@ -22,9 +17,10 @@ export type FlexProps = React.HTMLAttributes<HTMLDivElement> & {
     justify?: 'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
     gap?: SpaceSize;
     wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
-    width?: BoxSize | string;
-    height?: BoxSize | string;
+    width?: BoxSize | string | number;
+    height?: BoxSize | string | number;
     padding?: PaddingSize;
+    s?: boolean;
 };
 
 const Flex = ({
@@ -39,41 +35,18 @@ const Flex = ({
     style,
     className,
     padding,
+    s,
     ...props
 }: FlexProps) => {
-    const spacingToString = (padding: PaddingSize | number = 0): string => {
-        // now padding is always number/string/object
-
-        if (typeof padding === 'number' || typeof padding === 'string') {
-            return dimensionToSpace(padding as number | SpaceSize);
-        }
-
-        // object: start with x/y defaults
-        const px = padding.x != null ? dimensionToSpace(padding.x) : '0px';
-        const py = padding.y != null ? dimensionToSpace(padding.y) : '0px';
-
-        let top = py;
-        let bottom = py;
-        let left = px;
-        let right = px;
-
-        if (padding.t != null) top = dimensionToSpace(padding.t);
-        if (padding.b != null) bottom = dimensionToSpace(padding.b);
-        if (padding.l != null) left = dimensionToSpace(padding.l);
-        if (padding.r != null) right = dimensionToSpace(padding.r);
-
-        return `${top} ${right} ${bottom} ${left}`;
-    };
-
     const cssVariables: React.CSSProperties = {
         '--flex-direction': direction,
         '--align-items': align,
         '--justify-content': justify,
         '--wrap': wrap,
         '--gap': `var(--space-${gap})`,
-        '--width': dimensionToVariable(width),
-        '--height': dimensionToVariable(height),
-        '--padding': spacingToString(padding),
+        '--width': s ? dimensionToString(width) : dimensionToVariable(width),
+        '--height': s ? dimensionToString(height) : dimensionToVariable(height),
+        '--padding': s ? spacingToString(padding) : spacingToSpace(padding),
     } as React.CSSProperties;
 
     return (
