ae1c6ff5d893b8a24945ec3c33f354c97bf8ac8f
dhkim <dhkim@mleverage.co.kr>
2025-07-14 10:06:30 +0900

    feat: TooltipA, TooltipD.Content 수정 - Content 스타일 적용

--- src/components/contents/Tooltip/A/Content/Content.tsx
+++ src/components/contents/Tooltip/A/Content/Content.tsx
@@ -1,11 +1,39 @@
 import Tooltip from '@/headless/Tooltip/Tooltip';
 import React from 'react';
 import styles from './Content.module.scss';
+import { BoxSize, CSSPropertiesWithVars, FontSize, HexColor, PaddingSize } from '@/ui-kit/src/types';
+import { dimensionToString, spacingToString } from '@/ui-kit/src/utils';
+import classNames from 'classnames';
 
-type TooltipContentProps = React.ComponentProps<typeof Tooltip.Content>;
+type TooltipContentProps = React.ComponentProps<typeof Tooltip.Content> & {
+    width?: BoxSize | string | number;
+    height?: BoxSize | string | number;
+    padding?: PaddingSize;
+    fontSize?: FontSize | number;
+    bgColor?: HexColor;
+    borderColor?: HexColor;
+    color?: HexColor;
+};
 
-const Content = ({ ...props }: TooltipContentProps) => {
-    return <Tooltip.Content {...props} className={styles.Content} />;
+const Content = ({ width, height, padding, fontSize, bgColor, borderColor, color, ...props }: TooltipContentProps) => {
+    const cssVariables: CSSPropertiesWithVars = {
+        '--width': dimensionToString(width),
+        '--height': dimensionToString(height),
+        '--padding': spacingToString(padding),
+        '--font-size': dimensionToString(fontSize),
+        '--background-color': bgColor,
+        '--border-color': borderColor,
+        '--color': color,
+    };
+
+    const className = classNames(styles.Content, {
+        [styles.Left]: props.placement === 'left',
+        [styles.Right]: props.placement === 'right',
+        [styles.Top]: props.placement === 'top',
+        [styles.Bottom]: props.placement === 'bottom',
+    });
+
+    return <Tooltip.Content {...props} className={className} style={{ ...cssVariables }} />;
 };
 
 export default Content;
--- src/components/contents/Tooltip/D/Content/Content.tsx
+++ src/components/contents/Tooltip/D/Content/Content.tsx
@@ -1,23 +1,29 @@
 import Tooltip from '@/headless/Tooltip/Tooltip';
 import React from 'react';
 import styles from './Content.module.scss';
-import { BoxSize, CSSPropertiesWithVars } from '@/ui-kit/src/types';
-import { dimensionToVariable } from '@/ui-kit/src/utils';
+import { BoxSize, CSSPropertiesWithVars, FontSize, HexColor, PaddingSize } from '@/ui-kit/src/types';
+import { dimensionToString, spacingToString } from '@/ui-kit/src/utils';
 
 type TooltipContentProps = React.ComponentProps<typeof Tooltip.Content> & {
-    width?: BoxSize | string;
-    height?: BoxSize | string;
+    width?: BoxSize | string | number;
+    height?: BoxSize | string | number;
+    padding?: PaddingSize;
+    fontSize?: FontSize | number;
+    bgColor?: HexColor;
+    borderColor?: HexColor;
+    color?: HexColor;
 };
 
-const Content = ({ width = '100%', height = '100%', ...props }: TooltipContentProps) => {
-    console.log('왜이러지?', dimensionToVariable(width));
-
+const Content = ({ width, height, padding, fontSize, bgColor, borderColor, color, ...props }: TooltipContentProps) => {
     const cssVariables: CSSPropertiesWithVars = {
-        '--width': dimensionToVariable(width),
-        '--height': dimensionToVariable(height),
+        '--width': dimensionToString(width),
+        '--height': dimensionToString(height),
+        '--padding': spacingToString(padding),
+        '--font-size': dimensionToString(fontSize),
+        '--background-color': bgColor,
+        '--border-color': borderColor,
+        '--color': color,
     };
-
-    console.log('왜??', cssVariables);
 
     return <Tooltip.Content {...props} className={styles.Content} style={{ ...cssVariables }} />;
 };
