4604e2457f52b9cd52e07fc5526292c257feaf2b
dhkim <dhkim@mleverage.co.kr>
2025-07-21 10:38:17 +0900

    feat: Content 수정 - maxHeight 속성 설정

--- src/components/contents/Select/B/Content/Content.tsx
+++ src/components/contents/Select/B/Content/Content.tsx
@@ -2,21 +2,32 @@ import React from 'react';
 import Dropdown, { useDropdown } from '@/headless/Dropdown/Dropdown';
 import styles from './Content.module.scss';
 import classNames from 'classnames';
-import { ContentSize, CSSPropertiesWithVars } from '@/ui-kit/src/types';
+import { BoxSize, CSSPropertiesWithVars } from '@/ui-kit/src/types';
 
 type ContentProps = {
     children: React.ReactNode;
+    maxHeight?: BoxSize | string | number;
 };
 
-const Content = ({ children }: ContentProps) => {
+const Content = ({ children, maxHeight }: ContentProps) => {
     const { dropdownValue } = useDropdown();
+
+    const cssVariables: CSSPropertiesWithVars = {
+        '--max-height': `${maxHeight}px`,
+    };
 
     const combinedStyle = classNames(styles.Content, {
         [styles.Open]: dropdownValue, // dropdownValue가 true일 때 Open 클래스 적용
         [styles.Closed]: !dropdownValue, // dropdownValue가 false일 때 Closed 클래스 적용
     });
 
-    return <Dropdown.Content className={combinedStyle}>{children}</Dropdown.Content>;
+    console.log('맥스', maxHeight);
+
+    return (
+        <Dropdown.Content className={combinedStyle} style={{ ...cssVariables }}>
+            {children}
+        </Dropdown.Content>
+    );
 };
 
 export default Content;
