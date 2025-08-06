a615258f05628f8b1bb527ff35a8a7401dddef34
mleverage-Intern <intern@mleverage.co.kr>
2025-08-06 09:21:26 +0900

    templates 추가

---
+++ src/components/templates/Card/W/cardW.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './cardW.module.scss';
+import Image from '@/ui-kit/src/components/contents/Image/Image';
+import { BoxSize } from '@/ui-kit/src/types';
+
+type cardWProps = {
+    /**
+     * `true`면 숫자 타입 `iconWidth`를 px 단위 문자열로 변환하여 전달,
+     * `false`면 `BoxSize` 타입으로 전달
+     */
+    s?: boolean;
+    iconWidth?: BoxSize | number;
+    icon: string;
+    content?: React.ReactNode;
+};
+
+const CardW = ({ s = false, icon, content, iconWidth }: cardWProps) => {
+    // s가 true일 때 숫자는 '${px}' 문자열로, false일 때는 BoxSize로 취급
+    const widthValue: string | BoxSize | undefined = s
+        ? typeof iconWidth === 'number'
+            ? `${iconWidth}px`
+            : iconWidth
+        : (iconWidth as BoxSize | undefined);
+    return (
+        <div className={styles.container}>
+            <Image width={widthValue} image={icon} />
+            <div className={styles.flex}>{content}</div>
+        </div>
+    );
+};
+export default CardW;
