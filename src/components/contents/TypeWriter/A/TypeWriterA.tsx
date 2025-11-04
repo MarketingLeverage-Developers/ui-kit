3036c030b5726cc4153c77a2f7bf110221707f14
mleverage-Intern-kwonkibeom <krshlee22@mleverage.co.kr>
2025-11-04 18:21:44 +0900

    feat: TypeWritter 수정

--- src/components/contents/TypeWriter/A/TypeWriterA.tsx
+++ src/components/contents/TypeWriter/A/TypeWriterA.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { useEffect, useState } from 'react';
 import Text, { TextProps } from '@/ui-kit/src/components/contents/Text/Text';
-import styles from './TypewriterA.module.scss';
 import { FontSize, FontWeight } from '@/ui-kit/src/types';
+import styles from './TypeWriterA.module.scss';
 
 type TypewriterAPhase = 'typing' | 'pausing' | 'deleting' | 'completed';
 
