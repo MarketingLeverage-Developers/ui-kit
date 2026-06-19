3b9dbf40b902a5cdbf9ad8be91fda689178f241f
mleverage-Intern <intern@mleverage.co.kr>
2026-06-19 17:14:21 +0900

    fix: space 추가

--- src/theme/defaultVariables.ts
+++ src/theme/defaultVariables.ts
@@ -55,7 +55,7 @@ export const defaultVariables: Record<string, string> = {
     '--space-130': 'clamp(65px, calc(130 / var(--inner) * 100vw), 130px)',
     '--space-150': 'clamp(75px, calc(150 / var(--inner) * 100vw), 150px)',
     '--space-180': 'clamp(90px, calc(180 / var(--inner) * 100vw), 180px)',
-    '--space-200': 'clamp(100px, calc(150 / var(--inner) * 100vw), 200px)',
+    '--space-200': 'clamp(100px, calc(200 / var(--inner) * 100vw), 200px)',
 
     // --box sizes
     '--box-10': 'clamp(6px, calc(10 / var(--inner) * 100vw), 10px)',
